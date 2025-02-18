3029e7b6ba7afad53d069adb747b5652969bb12c
TheoHuetQC <theo6huet@gmail.com>
2025-02-18 21:43:51 +0100

    bords periodique fonctionne correctement et ajout d une vidéo démonstration du code

--- lennard-jones-potential.py
+++ lennard-jones-potential.py
@@ -11,24 +11,25 @@ G = 0 #gravitation
 K = 0 #frottement
 M = 1 #masse dune particule
 E = 1 #coefficient d'élasticité des rebonds, 1 si choque élastique (0 < E <= 1)
-T = 1 # Temperature
+T = 2 # Temperature
 
 #constante du problème :
 L  = 10 #taille de la boite
-NbrPart = 200 #nombre de particule
+NbrPart = 100 #nombre de particule
 D = 2 #dimention du probléme
 
 #Boundry condition :
-REBOND = True #la particule rebondi sur les parroies True = oui, False = non (et les particules on des conditions aux bords periodique)
+REBOND = False #la particule rebondi sur les parroies True = oui, False = non (et les particules on des conditions aux bords periodique)
 
 #constante de numérisation :
 N = 5000 #pour la precision de Verlet
 TEMPS = 1 #temps que l'on simule
 EPSILON = TEMPS/N #epsilon dans Verlet
+THERMALISATION_TIME = 5000
 
 #pour l'aniamtion :
 save_frames = True  #si on fait une annimation
-save_animation = False #si on sauvegarde l'animation sur la machine
+save_animation = True #si on sauvegarde l'animation sur la machine
 animation_interval = 5  #Intervalle pour l'animation (tout les combiens de step on sauvegarde les positions)
 
 ############################### fonctions ###############################
@@ -39,7 +40,7 @@ def animate_trajectory(positions_for_animation, L): #Animation des trajectoires 
     ax.set_xlim(0, L)
     ax.set_ylim(0, L)
     #le titre depend des paramametres de depart
-    title = "Potentiel de Lennard-Jones"
+    title = "Potentiel de Lennard-Jones\n"
     title += " avec rebonds" if REBOND else " avec conditions periodiques aux bords"
     title += ", (N = " + str(NbrPart) + ")"
     title += ", \nGravité (G = " + str(G) + ")" if (G != 0) else "\n"
@@ -91,17 +92,49 @@ def init_lattice() :
     vy -= np.mean(vy)
     v = np.column_stack((vx, vy))
     position_before = position - EPSILON * v #pour utiliser la methode de résolution d'equation diff de Verlet
-    return np.array(position), np.array(position_before)
+    position, position_before = thermalisation(np.array(position), np.array(position_before)) #on thermalise notre systeme
+    return position, position_before
 
-def lennardJ(r,rp) : #force de Lennard Jones entre la particule en position r et celle en position rp
-    """if r[0] - rp[0] > L/2 :
-        rp[0] = rp[0] - L
-    elif rp[0] - r[0] > L/2 :
-        rp[0]"""
-    if REBOND : # CA MARCHE PAS SANS REBOND
-        rij = r - rp
-    else :
-        rij = (r - rp)%L/2
+def simulate(position, position_before, j) :
+    vitesses = (position - position_before) / EPSILON #vitesse des particules
+    position_test = 2 * position - position_before + EPSILON * EPSILON * f(vitesses ,position, j * EPSILON) #calcule de la position d'apres avec Verlet
+    position_before = position #on conserve la position de la particule juste avant
+    
+    #Boundry Condition au choix :
+    
+    if REBOND : # rebond sur la paroie :
+        for i in range(NbrPart):  # Pour chaque particule
+            j = 0
+            for d in range(D) :
+                while position_test[i][d] > L or position_test[i][d] < 0 :
+                    if position_test[i][d] > L:  # Si la particule dépasse le bord supérieur
+                        position_test[i][d] = (1 + E) * L - E * position_test[i][d]  # Inverser la position
+                        position_before[i][d] = 2 * L - position_before[i][d]  # Inverser la vitesse 
+                    elif position_test[i][d] < 0:  # Si la particule dépasse le bord inférieur
+                        position_test[i][d] = - E * position_test[i][d]  # Inverser la position
+                        position_before[i][d] = - position_before[i][d]  # Inverser la vitesse
+                    j += 1
+                    if j > 10 :
+                        break
+        position = np.array(position_test)
+        
+    else : #bords periodiques
+        position = position_test%L #si par exemple x = L + 2 -> x = 2 pour rester dans la boite
+    return position, position_before
+
+def thermalisation(position, position_before) :
+    for t in range(THERMALISATION_TIME) :
+        position, position_before = simulate(position, position_before, t)
+    return position, position_before
+
+def lennardJ(ri,rj) : #force de Lennard Jones entre la particule en position r et celle en position rp
+    rij = ri - rj
+    if not(REBOND) :
+        for d in range(D):
+            if rij[d] >  L/2 :
+                rij[d] -= L
+            elif rij[d] < - L/2 :
+                rij[d] += L
     norme = np.linalg.norm(rij) #calcule de la norme au carré
     return 48 * EPS *(SIGMA**12 * norme**(-12)-1/2* SIGMA**6* norme*(-6))* rij / norme**2
 
@@ -127,7 +160,7 @@ def f(v, r ,t) : #fonction dans l'équadiff r" = f(r', r, t)
             else : #on calcule la force
                 force_LJ = lennardJ(r[i],r[j])
             interaction_forces_matrix[i].append(force_LJ) #+ d autre force d interaction si besoin
-        interaction_forces.append(np.sum(interaction_forces_matrix[i],axis=0))
+        interaction_forces.append(np.sum(interaction_forces_matrix[i],axis=0)) #on sommes la ligne i de la matrice = Sum Fij = Fi = Lennard jones appliqué sur i
         forces.append(gravity() + friction(v[i]) + interaction_forces[i])
     return np.array(forces)/M
 
@@ -139,30 +172,7 @@ positions_for_animation = [] #permet de stocker les positions que l'on souhaite 
 
 #calcul des trajectoires avec Verlet
 for j in range(2,N-1) :
-    vitesses = (position - position_before) / EPSILON #vitesse des particules
-    position_test = 2 * position - position_before + EPSILON * EPSILON * f(vitesses ,position, j * EPSILON) #calcule de la position d'apres avec Verlet
-    position_before = position #on conserve la position de la particule juste avant
-    
-    #Boundry Condition au choix :
-    
-    if REBOND : # rebond sur la paroie :
-        for i in range(NbrPart):  # Pour chaque particule
-            j = 0
-            for d in range(D) :
-                while position_test[i][d] > L or position_test[i][d] < 0 :
-                    if position_test[i][d] > L:  # Si la particule dépasse le bord supérieur
-                        position_test[i][d] = (1 + E) * L - E * position_test[i][d]  # Inverser la position
-                        position_before[i][d] = 2 * L - position_before[i][d]  # Inverser la vitesse 
-                    elif position_test[i][d] < 0:  # Si la particule dépasse le bord inférieur
-                        position_test[i][d] = - E * position_test[i][d]  # Inverser la position
-                        position_before[i][d] = - position_before[i][d]  # Inverser la vitesse
-                    j += 1
-                    if j > 10 :
-                        break
-        position = np.array(position_test)
-        
-    else : #bords periodiques
-        position = position_test%L #si par exemple x = L + 2 -> x = 2 pour rester dans la boite
+    position, position_before = simulate(position, position_before, j)
 
     # stock la position pour l'animation
     if save_frames and (j % animation_interval == 0) : #pour ne pas stocker inutilement
