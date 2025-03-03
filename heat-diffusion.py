29401d34249df66373eb26f7a11a99a82a01c2fa
Théo HUET <theo6huet@gmail.com>
2025-03-03 17:52:26 +0100

    Update heat-diffusion.py

--- heat-diffusion.py
+++ heat-diffusion.py
@@ -2,18 +2,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.animation as animation
 
-Nx = 100
-Nt = 100000
+############################### paramètres ###############################
+
+#constante physique :
+D = 1 #coefficient de diffusion
+
+#constante du problème :
 L = 10
-TEMPS = 100
+TEMPS = 10
+
+#Boundry condition :
+THERMOSTAT = True
+Tl = 5 #température du Thermostat a gauche
+Tr = 0 # " a droite
+
+#constante de numérisation :
+Nx = 100 
+Nt = 100000 #pas de temps
 dx = L/Nx
 dt = TEMPS/Nt
-D = 1
-etha = D*dt/(dx*dx)
+etha = D*dt/(dx*dx) #dans l'équation de diffusion
 
-intervalTime = 100
-save_animation = False
-save_frames = True
+#pour l'aniamtion :
+animation_interval = 100  #Intervalle pour l'animation (tout les combiens de step on sauvegarde les positions)
+save_animation = False #si on sauvegarde l'animation sur la machine
+save_frames = True  #si on fait une annimation
 
 def animate_trajectory(positions_for_animation, L): #Animation des trajectoires de particule
     fig, ax = plt.subplots(figsize=(6,6))
@@ -48,22 +61,37 @@ def animate_trajectory(positions_for_animation, L): #Animation des trajectoires 
         plt.close(fig)
     else :
         plt.show()
-        
-#init
+
+def T(x) : #valeurs de la température au depart
+    return np.cos(x) + np.sin(x + 2) + 2 
+
+#initialisation
 positions_for_animation = []
+
 u = np.zeros((Nt,Nx))
 for i in range(Nx) :
-    u[0, i] = np.cos(i*dx) + np.sin(i*dx +2) +2 
+    u[0, i] = T(i*dx)
 
 #compute
 for n in range(Nt-1) :
     positions = []
     for i in range(1,Nx-1) :
+        
+        #boundry condition
+        if THERMOSTAT : #Dirichlet condition (Thermostat au bord a température Tleft et Tright)
+            u[n+1, 0] = Tl
+            u[n+1, Nx-1] = Tr
+        else : #Neumann condition
+            u[n+1, 0] = Tl
+            u[n+1, Nx-1] = Tr
+        
+        #resoud la PDE
         u[n+1,i] = u[n,i] + etha * (u[n,i+1] - 2 * u[n,i] + u[n,i-1])
         positions.append([i*dx, u[n,i]])
-    if n%intervalTime == 0 : 
+        
+    if n % animation_interval  == 0 : #on sauvegarde les positions pour les annimées tout les "animation_interval" pas 
         positions_for_animation.append(positions)
-            
+
 # lance l'animation
 if save_frames and len(positions_for_animation) > 1 :
     animate_trajectory(positions_for_animation, L)
